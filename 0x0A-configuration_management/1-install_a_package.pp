591c33873498e707448d1011b4df2a4164104408
Constance Amatey <consamate57@gmail.com>
2023-11-24 23:26:35 +0000

    install flask

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# Installs install flask from pip3 using puppet
+
+package { 'flask':
+  ensure   =>  '2.1.0',
+  provider =>  'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
