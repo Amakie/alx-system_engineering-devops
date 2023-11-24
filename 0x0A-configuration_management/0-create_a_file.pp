5e9ab95cdb4608b8930b43501df276d1e8a5ac4f
Constance Amatey <consamate57@gmail.com>
2023-11-24 22:35:06 +0000

    create puppet file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-#Creates a puppet create a file in /tmp
+# Creates a puppet create a file in /tmp
+
 file { '/tmp/school':
-    ensure      =>  file,
-    content     => 'I love Puppet',
-    mode        => '0744'
-    owner       => 'www-data'
-    group       => 'www-data'
+    path        =>  '/tmp/school',
+    content     =>  'I love Puppet',
+    mode        =>  '0744'
+    owner       =>  'www-data'
+    group       =>  'www-data'
 }
