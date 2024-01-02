29c694667aeb5014764a15bb0347fed8dc76eba5
Constance Amatey <consamate57@gmail.com>
2024-01-02 23:21:08 +0000

    extend Python script to export data in the CSV format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+extend Python script to export data in the CSV format
+"""
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        req = "https://jsonplaceholder.typicode.com/users/{}/todos"\
+              .format(sys.argv[1])
+        response = requests.get(req).json()
+        req_use = "https://jsonplaceholder.typicode.com/users/{}"\
+                  .format(sys.argv[1])
+        info_user = requests.get(req_use).json()
+        name_user = info_user.get('username')
+        with open('{}.csv'.format(sys.argv[1]), 'w') as f:
+            writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+            for content in response:
+                id_us = content['userId']
+                complet = content['completed']
+                content = content['title']
+                writer.writerow([id_us, name_user, complet, content])
