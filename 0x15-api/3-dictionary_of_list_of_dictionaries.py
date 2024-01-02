1d47a18855860b654dbd62acde89d961f08beb02
Constance Amatey <consamate57@gmail.com>
2024-01-02 23:33:03 +0000

    extend Python script to export data in the JSON format

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+extend your Python script to export data in the JSON format
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    req = "https://jsonplaceholder.typicode.com/todos"
+    response = requests.get(req).json()
+    d = {}
+    req_user = "https://jsonplaceholder.typicode.com/users"
+    users = requests.get(req_user).json()
+    for user in users:
+        reso_todos = "https://jsonplaceholder.typicode.com/users/{}/todos"\
+              .format(user['id'])
+        rq = requests.get(reso_todos).json()
+        list_tasks = []
+        for content in rq:
+            d_task = {}
+            d_task['task'] = content['title']
+            d_task['completed'] = content['completed']
+            d_task['username'] = user['username']
+            list_tasks.append(d_task)
+        d[user['id']] = list_tasks
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(d, f)
