87885532cf29ab14575f8b07fba8d922cc0e875a
Constance Amatey <consamate57@gmail.com>
2024-01-19 23:12:06 +0000

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,34 @@
 #!/usr/bin/python3
 """
-    Function that request api reddit and return number of subscribers
-    for a specific subreddit
+function that queries the Reddit API and returns the number of subscribers
 """
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'user-agent': 'my-app/0.0.1'}
-    result = requests.get(url, headers=headers)
-    if result.status_code == 404:
+    """
+    Queries the Reddit API for the number of subscribers for a subreddit.
+
+    Subreddit is passed as an argument.
+    If the subreddit is not valid, 0 is returned.
+    """
+    # set variables
+    request_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'user-agent':
+               'chrome:number_of_subscribers_function:v1 (by /u/remyjuke)'}
+
+    response = requests.get(request_url,
+                            headers=headers,
+                            allow_redirects=False)
+
+    response_code = response.status_code
+    if (response_code >= 300):
         return 0
-    return result.json()['data']['subscribers']
+
+    subscriber_count = response.json().get("data").get("subscribers")
+    return subscriber_count
+
+if __name__ == "__main__":
+    number_of_subscribers("all")
+    number_of_subscribers("fake_subreddit")
